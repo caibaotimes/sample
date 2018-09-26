e7eee28d8bea94e218032cd316c7474bf8eafde6
caibaotimes <tutucheyouhui@sina.com>
2018-09-26 08:40:45 +0000

    finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title','sample') - laravel新手教程</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
