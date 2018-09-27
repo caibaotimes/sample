eab21ff9ce53544770f325ab97e9ce7a61018c8e
caibaotimes <tutucheyouhui@sina.com>
2018-09-27 02:30:21 +0000

    finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,9 +2,17 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>@yield('title','sample') - laravel新手教程</title>
+    <title>@yield('title','Sample App') - laravel新手教程</title>
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
 </head>
 <body>
-    @yield('content')
+    @include('layouts._header')
+    <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+            @yield('content')
+            @include('layouts._footer')
+        </div>
+    </div>
+
 </body>
 </html>
